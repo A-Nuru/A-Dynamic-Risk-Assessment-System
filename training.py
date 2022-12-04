5e690dc4e3255a773c5e8e102626420424ccf927
Your Name <you@example.com>
2022-12-04 00:31:50 +0000

    saving model and encoder

--- training.py
+++ training.py
@@ -2,6 +2,7 @@ from flask import Flask, session, jsonify, request
 import pandas as pd
 import numpy as np
 import pickle
+from joblib import dump
 import os
 from sklearn import metrics
 from sklearn.model_selection import train_test_split
@@ -22,9 +23,9 @@ def train_model():
     df_x, df_y, encoder = preprocess_data(df, None)
     x_train, x_test, y_train, y_test = train_test_split(df_x, df_y, test_size=0.20)
     
-    LogisticRegression(C=1.0, class_weight=None, dual=False, fit_intercept=True,
+    model = LogisticRegression(C=1.0, class_weight=None, dual=False, fit_intercept=True,
                     intercept_scaling=1, l1_ratio=None, max_iter=100,
-                    multi_class='warn', n_jobs=None, penalty='l2',
+                    multi_class='ovr', n_jobs=None, penalty='l2',
                     random_state=0, solver='liblinear', tol=0.0001, verbose=0,
                     warm_start=False)
     
@@ -34,5 +35,10 @@ def train_model():
     print(model.score(x_train, y_train))
     print(model.score(x_test, y_test))
     
+    #write the trained model and encoder to your workspace in a file called trainedmodel.pkl
     #write the trained model to your workspace in a file called trainedmodel.pkl
-
+    dump(model, os.path.join(model_path, "trainedmodel.pkl"))
+    dump(encoder, os.path.join(model_path, "encoder.pkl"))
+    
+if __name__ == "__main__":
+    train_model()
