14a4b74bfccc6e54b645b3b5cfa0247d81a97de5
Your Name <you@example.com>
2022-12-04 00:57:27 +0000

    loading the saved model and encoder

--- scoring.py
+++ scoring.py
@@ -9,9 +9,8 @@ from sklearn.linear_model import LogisticRegression
 import json
 
 
-
-#################Load config.json and get path variables
-with open('config.json','r') as f:
+#Load config.json and gt path variables
+with open('config:
     config = json.load(f) 
 
 dataset_csv_path = os.path.join(config['output_folder_path']) 
@@ -20,6 +19,13 @@ test_data_path = os.path.join(config['test_data_path'])
 
 #################Function for model scoring
 def score_model():
-    #this function should take a trained model, load test data, and calculate an F1 score for the model relative to the test data
+    """Function take a trained model, load test data, and calculate an F1 score for the model relative to the test data
     #it should write the result to the latestscore.txt file
+    Input: trained model
+    Output: latestscore.txt """
+    model = load(os.path.join(model_path, "trainedmodel.pkl"))
+    encoder = load(os.path.join(model_path, "encoder.pkl"))
+    
 
+if __name__ == "__main__":
+    score_model()
