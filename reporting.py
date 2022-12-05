14e48b753cdbfed78da70e29def6f81e7aa6bfcf
Your Name <you@example.com>
2022-12-05 14:55:15 +0000

    plotting and saving  the confusion matrix

--- reporting.py
+++ reporting.py
@@ -22,7 +22,16 @@ def score_model():
     Function for reporting - calculate a confusion matrix using the test data and the deployed model, and write the confusion matrix to the workspace"""
     y_pred, df_y = model_predictions(None)
     df_cm = metrics.confusion_matrix(df_y, y_pred)
-    
+    fig, ax = plt.subplots(figsize=(7.5, 7.5))
+    ax.matshow(df_cm, cmap=plt.cm.Blues, alpha=0.3)
+    for i in range(df_cm.shape[0]):
+        for j in range(df_cm.shape[1]):
+            ax.text(x=j, y=i,s=df_cm[i, j], va='center', ha='center', size='xx-large')
+ 
+    plt.xlabel('Predictions', fontsize=18)
+    plt.ylabel('Actuals', fontsize=18)
+    plt.title('Confusion Matrix', fontsize=18)
+    plt.savefig(os.path.join(model_path, "confusionmatrix.png"))
 
 if __name__ == '__main__':
     score_model()
